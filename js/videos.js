902b2ff18a7f7629b8080b50e250554ec452d46f
Eber Danza <eberdanzaweb@gmail.com>
2026-02-26 01:22:16 -0300

    tut

--- js/videos.js
+++ js/videos.js
@@ -7,12 +7,13 @@ document.getElementById("videos-container");
 const videos = [
 
 {
-youtubeId: "Weov97w93-4",
-title: "Video de prueba",
-description: "Descripción del video",
-date: "2026-02-26",
-thumbnail: "https://img.youtube.com/vi/Weov97w93-4/hqdefault.jpg"
+  "youtubeId": "Weov97w93-4",
+  "title": "¿POR QUE ODIAN A ADAM SANDLER? - SUFICIENTE INTERNET",
+  "description": "¿De dónde sale el odio hacia Adam Sandler?\n¿Por qué un actor que solo quiere hacer reír genera tanta bronca en internet?\nEn este video analizo el fenómeno detrás del “odio gratuito” a Sandler:\nsu estilo, su humor, su lealtad a sus amigos y por qué su felicidad simple incomoda tanto a cierta parte del público.💛 Mi nombre es Eber Danza y esto es Suficiente Internet: un lugar donde hay un tipo opinando de cosas.\n",
+  "date": "2026-02-01",
+  "thumbnail": "https://img.youtube.com/vi/Weov97w93-4/hqdefault.jpg"
 },
+
 
 // pegás más videos acá
 
